860693709dfb63ab8c98db46ac0196a8e379859f
bruno.araujo <bruno.araujo@accurate.com.br>
2020-01-13 12:19:51 -0300

    Updates

---
+++ precache-manifest.f2b31d7c8998543100efe54d6e7ec5aa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f1af261e24fd7342a527b04c14e2ad9",
+    "url": "/condominio/index.html"
+  },
+  {
+    "revision": "260e1feb078c38665624",
+    "url": "/condominio/static/css/main.7c9c3290.chunk.css"
+  },
+  {
+    "revision": "2d57f151901714be014a",
+    "url": "/condominio/static/js/2.7e5f57f9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/condominio/static/js/2.7e5f57f9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "260e1feb078c38665624",
+    "url": "/condominio/static/js/main.5e9599bc.chunk.js"
+  },
+  {
+    "revision": "5b0bbdc136c8879f8831",
+    "url": "/condominio/static/js/runtime-main.2530bf0b.js"
+  },
+  {
+    "revision": "de4dcd7a3000d6a4cb95f829a0891fa2",
+    "url": "/condominio/static/media/foto6.de4dcd7a.jpg"
+  },
+  {
+    "revision": "47cc5144ccfd6c272138454742257dd5",
+    "url": "/condominio/static/media/wa1.47cc5144.gif"
+  }
+]);
