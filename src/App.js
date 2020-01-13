a830780bf07a67a9f0c8efd3afce60b325189deb
bruno.araujo <bruno.araujo@accurate.com.br>
2020-01-13 15:05:43 -0300

    evidencias

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import React , {useState} from 'react';
+import React from 'react';
 import logo from './midia/foto6.jpg';
 import './App.css';
 import 'gridlex'
 
 const App = () => {
-  const [pending,setPending] = useState([
+  const pending = [
     {
       name:'vidro hall t-7 danificado',
       action:'trocar o vidro de uma porta',
-      starttedAt:'2019-12-01'
-    }
-  ])
+      starttedAt:'2019-12-01',
+      link:'https://drive.google.com/open?id=1n-P0vZSmp0HOX3YVtU_P7i_GFUcFF4p5'
+    },
+    {
+      name:'implantação de segurança 100%',
+      action:'seguir o cronograma',
+      starttedAt:'2019-7-31',
+      link:'https://drive.google.com/open?id=1JAwAvOR4kUPsKzFeu0CWbr-kTZBCzTb2'
+    },
+    {
+      name:'grelhas das churrasqueiras',
+      action:'Trocar as custas de quem danificou',
+      starttedAt:'2018-12-28',
+      link:'https://drive.google.com/open?id=1nVJq-AVzrEXFu7CWcxxKF7hxsixE2Qzr'
+    },
+  ]
 
   const date = (dateString) => {
     let date = new Date(dateString)
@@ -35,20 +48,22 @@ const App = () => {
         <ul>
           {
             pending.map((obj,key)=>
-              <li className="grid col-12" key={key}>
-                <div className="col-4_sm-12">
+              <li className="grid" key={key}>
+                <div className="col-2_sm-12">
                   <label>#{key+1}</label>
                 </div>
                 <div className="col-4_sm-12">
                   <label>item:<b>{obj.name}</b></label>
-                  <label>data do problema:<b>{date(obj.starttedAt)}</b></label>
+                  <label>data:<b>{date(obj.starttedAt)}</b></label>
                   <label>Ação: <b>{obj.action}</b></label>
                 </div>
                 <div className="col-4_sm-12">
                   <label>{waiting(obj.starttedAt)}</label>
-                  <img src={require('./midia/wa1.gif')}/>  
+                  <img src={require('./midia/wa'+(key+1)+'.gif')}/>  
                 </div>
-
+                <div className="col-2_sm-12">
+                  <label><a href={obj.link} target="_blank">evidencia</a></label>
+                </div>
               </li>
             )
           }
